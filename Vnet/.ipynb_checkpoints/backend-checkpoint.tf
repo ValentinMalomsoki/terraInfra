28acef482c2501730c3deee5328d7882078bc048
Ubuntu <student@valentin-enablon.1nkoirkcj1cuzicxtpoxj5ixyf.parx.internal.cloudapp.net>
2025-05-21 15:12:19 +0000

    fix: files

--- Vnet/.ipynb_checkpoints/backend-checkpoint.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "val-rg"
-    storage_account_name = "mystorevalfo"
-    container_name       = "statestore"
-    key                  = "terraform.tfstate"
-  }
-}
